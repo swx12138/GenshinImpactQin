d71797ac706737e5756cedf1065a997522e71f5d
zhaolc_bzh <zhaolc_bzh@163.com>
2021-03-30 21:33:04 +0800

    小修改；默认播放小星星

--- main.cpp
+++ main.cpp
@@ -6,174 +6,169 @@
 
 using namespace std;
 
-class Qin
-{
-	enum class MusicNoteH :unsigned
+namespace GenshinImpact {
+	class Qin
 	{
-		Do = 'Q',
-		Re = 'W',
-		Mi = 'E',
-		Fa = 'R',
-		So = 'T',
-		La = 'Y',
-		Si = 'U'
-	};
-	enum class MusicNoteM :unsigned
-	{
-		Do = 'A',
-		Re = 'S',
-		Mi = 'D',
-		Fa = 'F',
-		So = 'G',
-		La = 'H',
-		Si = 'J'
-	};
-	enum class MusicNoteL :unsigned
-	{
-		Do = 'Z',
-		Re = 'X',
-		Mi = 'C',
-		Fa = 'V',
-		So = 'B',
-		La = 'N',
-		Si = 'M'
-	};
-public:
-	Qin(ifstream &ifs) :
-		errorCode(0)
-	{
-		HWND hWnd = ::FindWindowA("UnityWndClass", "原神");
-		if(!hWnd)
+		constexpr static const int pai = 555;
+		enum class MusicNoteH :unsigned
 		{
-			errorCode = 1;
+			Do = 'Q',
+			Re = 'W',
+			Mi = 'E',
+			Fa = 'R',
+			So = 'T',
+			La = 'Y',
+			Si = 'U'
+		};
+		enum class MusicNoteM :unsigned
+		{
+			Do = 'A',
+			Re = 'S',
+			Mi = 'D',
+			Fa = 'F',
+			So = 'G',
+			La = 'H',
+			Si = 'J'
+		};
+		enum class MusicNoteL :unsigned
+		{
+			Do = 'Z',
+			Re = 'X',
+			Mi = 'C',
+			Fa = 'V',
+			So = 'B',
+			La = 'N',
+			Si = 'M'
+		};
+	public:
+		Qin()
+		{
+			if(!getGenshinWnd())
+				return;
+
+			string xxx = "QQTTYYT RREEWWQ TTRREEW TTRREEW QQTTYYT RREEWWQ";
+			for(auto e : xxx)
+			{
+				play_main(e, 200);
+			}
+		}
+
+		Qin(ifstream &ifs)
+		{
+			if(!getGenshinWnd())
+				return;
+
+			if(!ifs)
+			{
+				errorCode = 2;
+				return;
+			}
+
+			string line;
+			while(getline(ifs, line))
+			{
+				if("name" == line.substr(0, 4))
+				{
+					cout << "name:" << line.substr(5) << endl;
+					continue;
+				}
+
+				if("from" == line.substr(0, 4))
+				{
+					cout << "from:" << line.substr(5) << endl;
+					continue;
+				}
+
+				for(auto e : line)
+				{
+					if((e >= 'a' && e <= 'z') || (e >= 'A' && e <= 'Z'))
+						play_main(static_cast<unsigned>(IsCharUpperA(e) ? e : (e - 'a' + 'A')), pai);
+				}
+
+				play_wait(pai);
+				cout << endl;
+			}
+
 			return;
-		}
-		::SetForegroundWindow(hWnd);
 
-		if(!ifs)
+		}
+
+		Qin(const Qin &) = delete;
+
+		~Qin()
 		{
-			errorCode = 2;
-			return;
+			::SetForegroundWindow(::GetConsoleWindow());
+			cout << "\nover" << endl;
 		}
 
-		string line;
-		while(getline(ifs, line))
+		operator int()
 		{
-			if("name" == line.substr(0, 4))
-			{
-				cout <<"name:"<< line.substr(5) << endl;
-				continue;
-			}
-
-			if("from" == line.substr(0, 4))
-			{
-				cout << "from:" << line.substr(5) << endl;
-				continue;
-			}
-
-			for(auto e : line)
-			{
-				if((e >= 'a' && e <= 'z') || (e >= 'A' && e <= 'Z'))
-					play(hWnd, static_cast<unsigned>(IsCharUpperA(e) ? e : (e - 'a' + 'A')));
-				play(hWnd, 500, true);
-			}
-
-			play(hWnd, 500, true);
-			cout << endl;
+			return errorCode;
 		}
 
-		/*
-		//MusicNote a(MusicNote::H::Do);
-		play(hWnd, MusicNoteH::Do);
-		play(hWnd, 500, true);
-		play(hWnd, MusicNoteH::Do);
-		play(hWnd, 500, true);
-		play(hWnd, MusicNoteH::So);
-		play(hWnd, 500, true);
-		play(hWnd, MusicNoteH::So);
-		play(hWnd, 500, true);
-		play(hWnd, MusicNoteH::La);
-		play(hWnd, 500, true);
-		play(hWnd, MusicNoteH::La);
-		play(hWnd, 500, true);
-		play(hWnd, MusicNoteH::So);
-		play(hWnd, 1000, true);
+	private:
+		bool getGenshinWnd()
+		{
+			hGenshinWnd = ::FindWindowA("UnityWndClass", "原神");
+			if(!hGenshinWnd)
+			{
+				errorCode = 1;
+				return false;
+			}
 
-		play(hWnd, MusicNoteH::Fa);
-		play(hWnd, 500, true);
-		play(hWnd, MusicNoteH::Fa);
-		play(hWnd, 500, true);
-		play(hWnd, MusicNoteH::Mi);
-		play(hWnd, 500, true);
-		play(hWnd, MusicNoteH::Mi);
-		play(hWnd, 500, true);
-		play(hWnd, MusicNoteH::Re);
-		play(hWnd, 500, true);
-		play(hWnd, MusicNoteH::Re);
-		play(hWnd, 500, true);
-		play(hWnd, MusicNoteH::Do);
-		play(hWnd, 1000, true);
-		*/
+			if(!::SetForegroundWindow(hGenshinWnd))
+			{
+				errorCode = GetLastError();
+				return false;
+			}
 
-		return;
+			return true;
+		}
 
-	}
+		bool play(const MusicNoteH &h)
+		{
+			return play_main(static_cast<unsigned>(h), pai);
+		}
+		bool play(const MusicNoteM &m)
+		{
+			return play_main(static_cast<unsigned>(m), pai);
+		}
+		bool play(const MusicNoteL &l)
+		{
+			return play_main(static_cast<unsigned>(l), pai);
+		}
 
-	Qin(const Qin &) = delete;
+		bool play_main(unsigned note, unsigned delay)
+		{
+			cout << char(note);
 
-	~Qin()
-	{
-		::SetForegroundWindow(::GetConsoleWindow());
-		cout << "over" << endl;
-	}
+			::PostMessageA(hGenshinWnd, WM_KEYDOWN, note, 0x100001);
+			this_thread::sleep_for(chrono::milliseconds(30));
+			::PostMessageA(hGenshinWnd, WM_KEYUP, note, 0xc0100001);
+			this_thread::sleep_for(chrono::milliseconds(delay));
 
-	operator int()
-	{
-		return errorCode;
-	}
 
-private:
-	bool play(HWND hWnd, const MusicNoteH &h)
-	{
-		return play(hWnd, static_cast<unsigned>(h));
-	}
-	bool play(HWND hWnd, const MusicNoteM &m)
-	{
-		return play(hWnd, static_cast<unsigned>(m));
-	}
-	bool play(HWND hWnd, const MusicNoteL &l)
-	{
-		return play(hWnd, static_cast<unsigned>(l));
-	}
-	bool play(HWND hWnd, unsigned note)
-	{
-		cout << char(note);
+			return true;
+		}
 
-		::PostMessageA(hWnd, WM_KEYDOWN, note, 0x100001);
-		this_thread::sleep_for(chrono::milliseconds(30));
-		::PostMessageA(hWnd, WM_KEYUP, note, 0xc0100001);
-
-		return true;
-	}
-	bool play(HWND hWnd, unsigned delay, bool reserved)
-	{
-		cout << " ";
-		this_thread::sleep_for(chrono::milliseconds(delay));
-		return reserved;
-	}
-public:
-	int errorCode;
-
-};
+		void play_wait(unsigned delay)
+		{
+			cout << " ";
+			this_thread::sleep_for(chrono::milliseconds(delay));
+		}
+	public:
+		int errorCode = 0;
+		HWND hGenshinWnd;
+	};
+}
 
 int main(int argc, char **argv)
 {
 	if(argc <= 1)
 	{
-		cout << "缺少曲谱文件名" << endl;
-		return -1;
+		return GenshinImpact::Qin();
 	}
 
 	ifstream ifs(argv[1]);
-	return Qin(ifs);
+	return GenshinImpact::Qin(ifs);
 }
